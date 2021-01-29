07fd313eeb598dcba156d9c0fc138941cb13e757
NicholasSolo <ohispilledmybeer@gmail.com>
2021-01-29 13:39:52 +0300

    feat: added lesson 2 tasks | main js file

--- script/main.js
+++ script/main.js
@@ -1,10 +1,21 @@
-let money;
-let income;
-let addExpenses;
-let deposit;
-let mission;
-let period;
+let money = 20000;
+let income = "freelance";
+let addExpenses = "internet, travelling, hobbies, fastfood";
+let deposit = true;
+let mission = 100000;
+let period = 12;
 
-alert("сообщение с любым текстом");
+console.log(typeof money);
+console.log(typeof income);
+console.log(typeof deposit);
 
-console.log("сообщение с любым текстом");
+console.log("Длина строки addExpenses - " + addExpenses.length + " символов");
+
+console.log(`Период равен ${period} месяцев`);
+console.log(`Цель заработать ${mission} рублей`);
+
+console.log(addExpenses.toLowerCase().split(','));
+
+let budgetDay = money / 30;
+
+console.log("Дневной бюджет:", Math.trunc(budgetDay), "рублей");
