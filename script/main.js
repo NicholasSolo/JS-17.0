3c1f1dd4512e29f3329032f9e5150baa8125b64b
NicholasSolo <ohispilledmybeer@gmail.com>
2021-01-29 20:44:17 +0300

    fix: fixed types of variables | main.js file

--- script/main.js
+++ script/main.js
@@ -1,21 +1,18 @@
-let money = 20000;
-let income = "freelance";
-let addExpenses = "internet, travelling, hobbies, fastfood";
-let deposit = true;
-let mission = 100000;
-let period = 12;
+const money = 20000;
+const income = "freelance";
+const addExpenses = "internet, travelling, hobbies, fastfood";
+const deposit = true;
+const mission = 100000;
+const period = 12;
+const budgetDay = money / 30;
 
-console.log(typeof money);
-console.log(typeof income);
-console.log(typeof deposit);
+console.log(typeof money, typeof income, typeof deposit);
 
 console.log("Длина строки addExpenses - " + addExpenses.length + " символов");
 
 console.log(`Период равен ${period} месяцев`);
 console.log(`Цель заработать ${mission} рублей`);
 
-console.log(addExpenses.toLowerCase().split(','));
-
-let budgetDay = money / 30;
+console.log(addExpenses.toLowerCase().split(","));
 
 console.log("Дневной бюджет:", Math.trunc(budgetDay), "рублей");
