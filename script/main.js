80ffdf8706b8f77e099a3b63046a8668745f4cec
NicholasSolo <ohispilledmybeer@gmail.com>
2021-02-01 16:14:23 +0300

    feat: added task for lesson03 | main.js file

--- script/main.js
+++ script/main.js
@@ -1,18 +1,47 @@
-const money = 20000;
+"use strict";
+
+const money = +prompt("Ваш месячный доход?", "35000");
 const income = "freelance";
-const addExpenses = "internet, travelling, hobbies, fastfood";
-const deposit = true;
+const addExpenses = prompt("Перечислите возможные расходы за рассчитываемый период через запятую:",  "Путешествия, покупки, развлечения");
+const deposit = confirm("Есть ли у Вас депозит в банке?");
+
+const expenses1 = prompt("Введите обязательную статью расходов?", "ЖХК");
+const cost1 = +prompt("Во сколько это обойдется?", "10000");
+
+const expenses2 = prompt("Введите обязательную статью расходов?", "Проезд");
+const cost2 = +prompt("Во сколько это обойдется?", "2000");
+
+const budgetMonth = money - (cost1 + cost2);
+console.log('Бюджет на месяц: ', budgetMonth);
+
 const mission = 100000;
-const period = 12;
-const budgetDay = money / 30;
+console.log(`Цель заработать ${mission} рублей`);
+
+const period = Math.ceil(mission / budgetMonth);
+console.log("Цель будет достигнута за", period, "месяца(-ев)");
+
+const budgetDay = Math.floor(budgetMonth / 30);
+console.log("Бюджет на день:", budgetDay, "рублей");
+
+if (budgetDay > 1200) {
+    console.log("У Вас высокий уровень дохода");
+  } else if (budgetDay >= 600 && budgetDay <= 1200) {
+    console.log("У Вас средний уровень дохода");
+  } else if (budgetDay <= 600 && budgetDay >= 0) {
+    console.log("К сожалению у Вас уровень дохода ниже среднего");
+  } else if (budgetDay <= 0) {
+    console.log("Что-то пошло не так");
+  }
+
+
+console.log('----------------------------------------');
 
 console.log(typeof money, typeof income, typeof deposit);
 
 console.log("Длина строки addExpenses - " + addExpenses.length + " символов");
 
 console.log(`Период равен ${period} месяцев`);
-console.log(`Цель заработать ${mission} рублей`);
 
 console.log(addExpenses.toLowerCase().split(","));
 
-console.log("Дневной бюджет:", Math.trunc(budgetDay), "рублей");
+
