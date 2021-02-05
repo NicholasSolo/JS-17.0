9d2afbd16826f90264e173db7c53dd1a37e2c36d
NicholasSolo <ohispilledmybeer@gmail.com>
2021-02-05 13:48:01 +0300

    feat: added GuessNumberBot script | main.js file

--- script/main.js
+++ script/main.js
@@ -1,90 +1,38 @@
 "use strict";
 
-let money;
-const income = "фриланс";
-const addExpenses = prompt(
-  "Перечислите возможные расходы за рассчитываемый период через запятую:",
-  "Путешествия, покупки, развлечения"
-);
-const deposit = confirm("Есть ли у Вас депозит в банке?");
-
-const mission = 100000;
-
 function isNumber(number) {
   return !isNaN(parseFloat(number)) && isFinite(number);
 }
 
-function start() {
-  do {
-    money = prompt("Ваш месячный доход", "");
-  } while (!isNumber(money));
-}
+function guessNumber() {
+  let number = 34;
 
-start();
+  function guessNumberInner() {
+    let userAnswer = prompt("Угадайте число от 1 до 100!", "");
 
-function showTypeOf(variable) {
-  return typeof variable;
-}
-console.log(showTypeOf(money));
-console.log(showTypeOf(income));
-console.log(showTypeOf(deposit));
-
-const compulsoryExpenses = [];
-
-function getExpensesMonth() {
-  let sum = 0;
-
-  for (let i = 0; i < 2; i++) {
-    compulsoryExpenses.push(prompt("Введите обязательную статью расходов?"));
-
-    for (let j; !isNumber(j); ) {
-      j = prompt("Во сколько это обойдется?");    //можно было сделать и через do..while, но пока делал, увидел в общем чате обсуждение и решение, поэтому попробовал сделать иначе
-      console.log("j:", j);
-      if (isNumber(j)) {
-        sum += +j;
-      }
+    if (userAnswer === null) {
+      alert("Игра окончена");
+      return;
+    } else if (!isNumber(userAnswer) || userAnswer > 100 || userAnswer < 0) {
+      alert("Введите число от 1 до 100!");
+      guessNumberInner();
+    } else if (userAnswer == number) {
+      return alert(
+        "Угадали! Не хотите принять участие в новом сезоне 'Битвы экстрасенсов'?"
+      );
+    } else if (userAnswer > number) {
+      alert("Загаданное число меньше");
+      guessNumberInner();
+    } else if (userAnswer < number) {
+      alert("Загаданное число больше");
+      guessNumberInner();
+    } else {
+      alert("Что-то пошло не так");
+      return;
     }
   }
-  return sum;
+  return guessNumberInner;
 }
 
-let expensesAmount = getExpensesMonth();
-console.log("Сумма расходов за месяц:", expensesAmount);
-
-console.log(
-  "Перечень возможных расходов:",
-  addExpenses.toLowerCase().split(",")
-);
-
-function getAccumulatedMonth(earnedMoney) {
-  return earnedMoney - expensesAmount;
-}
-const accumulatedMonth = getAccumulatedMonth(money);
-
-function getTargetMonth(target, accumulated) {
-  let period = Math.ceil(target / accumulated);
-  if (period > 0) {
-    return `Цель будет достигнута за ${period} месяца(-ев)`;
-  } else {
-    return "Цель не будет достигнута";
-  }
-}
-console.log(getTargetMonth(mission, accumulatedMonth));
-
-const budgetDay = Math.floor(accumulatedMonth / 30);
-console.log("Бюджет на день:", budgetDay, "рублей");
-
-function getStatusIncome(budgetDay) {
-  if (budgetDay >= 1200) {
-    return "У Вас высокий уровень дохода";
-  } else if (budgetDay >= 600) {
-    return "У Вас средний уровень дохода";
-  } else if (budgetDay <= 600) {
-    return "К сожалению, у Вас уровень дохода ниже среднего";
-  } else if (budgetDay <= 0) {
-    return "Что-то пошло не так";
-  } else {
-    return "Ошибка";
-  }
-}
-console.log(getStatusIncome(budgetDay));
+let game = guessNumber();
+game();
