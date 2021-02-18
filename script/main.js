4a6d8d275b91762644604bdadc06af4faa08d277
NicholasSolo <ohispilledmybeer@gmail.com>
2021-02-18 12:43:31 +0300

    fix: fixes according to comments | main.js file

--- script/main.js
+++ script/main.js
@@ -1,38 +1,37 @@
 "use strict";
 
 function DomElement(params) {
-  this.selector = params.selector;
-  this.height = params.height;
-  this.width = params.width;
-  this.bg = params.bg;
-  this.fontSize = params.fontSize;
+  this.selector = params.selector || '.block';
+  this.height = params.height || '50px';
+  this.width = params.width || '50px';
+  this.bg = params.bg || 'blue';
+  this.fontSize = params.fontSize || '14px';
+  this.text = params.text || "Sorry, no text";
 }
 
 DomElement.prototype.generateElem = function () {
-  const parent = document.querySelector("#container");
+  let newElem;
 
   if (this.selector[0] === ".") {
-    const newElem = document.createElement("div");
+    newElem = document.createElement("div");
     newElem.classList.add(this.selector.slice(1));
-    newElem.innerHTML = "Some text";
-    newElem.style.cssText = `height: ${this.height};  width: ${this.width};  background-color: ${this.bg};  font-size: ${this.fontSize}`;
-    newElem.style.color = "red";
-    parent.append(newElem);
   } else if (this.selector[0] === "#") {
-    const newElem = document.createElement("div");
+    newElem = document.createElement("p");
     newElem.id = this.selector.slice(1);
-    newElem.innerHTML = "Some text";
-    newElem.style.cssText = `height: ${this.height};  width: ${this.width};  background-color: ${this.bg};  font-size: ${this.fontSize}`;
-    newElem.style.color = "red";
-    parent.append(newElem);
   }
+
+  newElem.textContent = this.text;
+  newElem.style.cssText = `height: ${this.height};  width: ${this.width};  background-color: ${this.bg};  font-size: ${this.fontSize}`;
+  document.querySelector("body").append(newElem);
 };
 
 const test = new DomElement({
-  selector: ".block",
+  selector: "#block",
   height: "100px",
-  width: "100px",
+  width: "fit-content",
   bg: "green",
   fontSize: "20px",
+  text: "Hello, world!"
 });
+
 test.generateElem();
